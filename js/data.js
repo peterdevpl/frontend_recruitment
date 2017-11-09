6cb5d0bd17f0b23102594caf4cc2bf11f0586d17
Vongriffe <you@example.com>
2017-11-09 11:50:23 +0100

    2.Dynamicznie zmieniany link do API

--- js/data.js
+++ js/data.js
@@ -1,3 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
+
+  var apiPath = "https://gwo.pl/booksApi/v1/search?query=";
+  var submit = document.getElementById("submit"); // Wyszukiwanie przycisku submit w DOM-ie
+
+  submit.addEventListener ("click", function(event) {  // dodanie eventu na przycisk submit
+    this.url =  apiPath +  encodeURI(userInput.value);
+    console.log(this.url); // sprawdzam w konsoli czy link jest dynamicznie zmieniany
+
+  });
+
   console.log("DOM fully loaded and parsed"); // test - załadowanie drzewa DOM
+
 });
