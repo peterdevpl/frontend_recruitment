5434aaec2ddac28eb54833b1bf59b941722dccb6
Vongriffe <you@example.com>
2017-11-20 22:48:21 +0100

    Sass footer

--- js/component.js
+++ js/component.js
@@ -1,3 +1,5 @@
+console.log(' world!');
+
 document.addEventListener("DOMContentLoaded", function() {
   console.log("DOM fully loaded and parsed");
 
@@ -19,34 +21,33 @@ document.addEventListener("DOMContentLoaded", function() {
   var footerLinkSecond = "http://capalgerie.org/portfolio/";
   var mailToLink = "mailto:kamelboukoffa.mac@gmail.com?";
 
-
-  console.log();
-
   function mailConcat(link, tag) {
+
     var content = "Subject=Zadanie rekrutacyjne frontend&";
+
     link+= "";
+
     if (link.indexOf("@") >=0 ) {
       content += "body=" + encodeURIComponent(formattedBody);
       tag.appendChild(emailText);
       tag.title = "napisz maila";
       tag.href = link + content;
+      return tag;
+
     } else if (link.indexOf("github") >=0 ) {
       tag.appendChild(footerTextFirst);
       tag.href = link;
       tag.target = '_blank';
+      return tag;
+
     } else if (link.indexOf("portfolio") >=0 ) {
       tag.appendChild(footerTextSecond);
       tag.href = link;
       tag.target = '_blank';
+      return tag;
     }
   }
   mailConcat(mailToLink, contactToMail);
   mailConcat(footerLinkFirst, gitHub);
   mailConcat(footerLinkSecond, portfolio);
-
-
-
-
-
-
 });
